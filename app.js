4ad159655afdae3eb2abae649ecff12dd78ee603
swapan Gupta <swapan.gupta45@gmail.com>
2021-01-18 16:22:47 +0530

    registration(seccond commit)

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 const express = require('express');
 const app = express();
 
-console.log("hello world");
+const bodyParser = require('body-parser');
+// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.post('/register', function (req, res) {
+    const fullName = req.body.fullName;
+    const email = req.body.email;
+    const address = req.body.address;
+    const password = req.body.password;
+    //const confirmPassword = req.body.confirmpassword;
+    //const equalValid = password === confirmPassword ? res.send("Password does match") : res.send("Password doesnot match");
+    const valid = fullName && email && address && password !== {} ? res.send("response successful") : res.send("response unsuccessful");
+    console.log(req.body);
+});
+app.listen(3005);
+console.log("Server Listening at 3005");
